5fdd86d950cb857b423301ea9fb0a9e64f95e9c5
John Quinnvic Taboada <johnquinnvic.taboada@cit.edu>
2023-12-04 14:28:00 +0800

    DamageScreen & DashScreen Pop

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -17,6 +17,7 @@ public class EnemyController : MonoBehaviour
 
     public bool playerIsAlive;
     public ScrollController scrollController;
+    public bool dropScroll = false;
     private Vector2 dropPosition;
 
     public bool isFlipped = false;
@@ -76,8 +77,7 @@ public class EnemyController : MonoBehaviour
     }
 
     public void RemoveEnemy(){
-        if(scrollController){
-
+        if(dropScroll){
             scrollController.DropScroll(dropPosition);
         }
         Destroy(gameObject);
