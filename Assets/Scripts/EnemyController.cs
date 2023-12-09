763a6d2d67fba196944b558c52cde27550120443
John Quinnvic Taboada <johnquinnvic.taboada@cit.edu>
2023-12-09 16:16:20 +0800

    Smol Change

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -97,7 +97,7 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate(){
         if(canMove){
-        if(detectionZone.detectedObj.Count > 0 && detectionZone){
+        if(detectionZone && detectionZone.detectedObj.Count > 0 && detectionZone){
             Vector2 direction = (detectionZone.detectedObj[0].transform.position - transform.position).normalized;
             Vector3 scale = transform.localScale;
             
